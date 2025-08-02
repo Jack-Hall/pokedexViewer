5c2e4fadc31ee6261e203772bcfb2137e62aa94a
Jack Hall <j28hall@uwaterloo.ca>
2025-08-01 22:20:32 -0400

    more more more

--- PokeDex/src/app/pokemon-list-item/pokemon-list-item.ts
+++ PokeDex/src/app/pokemon-list-item/pokemon-list-item.ts
@@ -2,6 +2,7 @@ import { Component,  OnInit } from '@angular/core';
 import { PokemonDataService, PokemonData } from '../data.service';
 import { PokemonCard } from '../pokemon-card/pokemon-card';
 import { Pokemon } from '../pokemon.types';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list-item',
@@ -12,22 +13,40 @@ import { Pokemon } from '../pokemon.types';
 
 export class PokemonList  implements OnInit {
   pokemons: Pokemon[] = []
+  selectedPokemon: Pokemon | null = null;
+
   constructor(
     private dataService: PokemonDataService
   )
   {
 
   }
+
   ngOnInit(): void {
     this.dataService.getPokemon()
     .subscribe((response: any)=> {
       console.log(response);
-      for(let i = 0; i < response.results.length; ++i){
-        this.dataService.getAllData(response.results[i].name).subscribe((response: any) => this.pokemons.push(response));
-      }
-      console.log(this.pokemons)
-      }
-  )
-    
+      
+      // Create an array of observables for all Pokemon data calls
+      const pokemonObservables = response.results.map((pokemon: any) => 
+        this.dataService.getAllData(pokemon.name)
+      );
+      
+      // Wait for all calls to complete, then sort
+      forkJoin(pokemonObservables).subscribe((pokemonData: unknown) => {
+        this.pokemons = (pokemonData as Pokemon[]).sort((poke_a: Pokemon, poke_b: Pokemon) => {
+          return poke_a.id - poke_b.id; // Sort by ID ascending
+        });
+        console.log('Sorted Pokemon:', this.pokemons);
+      });
+    });
+  }
+
+  onPokemonHover(pokemon: Pokemon): void {
+    this.selectedPokemon = pokemon;
+  }
+
+  onPokemonLeave(): void {
+    this.selectedPokemon = null;
   }
 }
